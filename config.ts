963ce6f44d4e760f07312ee6f4deefd49284a954
Jeff-Adler <Jeffm.adler@gmail.com>
2021-10-02 13:09:00 -0400

    created schema model

--- config.ts
+++ config.ts
@@ -8,43 +8,43 @@ const config = convict({
     doc: 'The application environment.',
     format: ['production', 'development', 'test'],
     default: 'development',
-    env: 'NODE_ENV',
+    env: 'NODE_ENV'
   },
   ip_address: {
     doc: 'The IP address to bind.',
     format: 'ipaddress',
     default: '127.0.0.1',
-    env: 'IP_ADDRESS',
+    env: 'IP_ADDRESS'
   },
   port: {
     doc: 'The port to bind.',
     format: 'port',
     default: 8080,
     env: 'PORT',
-    arg: 'port',
+    arg: 'port'
   },
   db: {
     database_url: {
       doc: 'URL for postgres DB',
       format: String,
       default: '',
-      env: 'DATABASE_URL',
-    },
+      env: 'DATABASE_URL'
+    }
   },
   cors: {
     origin: {
       doc: 'sets Access-Control-Allow-Origin',
       format: Boolean,
       default: true,
-      env: 'cors.origin',
+      env: 'cors.origin'
     },
     credentials: {
       doc: 'sets Access-Control-Allow-Credentials',
       format: Boolean,
       default: true,
-      env: 'cors.credentials',
-    },
-  },
+      env: 'cors.credentials'
+    }
+  }
 });
 
 const env = config.get('env');
