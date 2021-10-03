f5f4488a4316e0190c3b4d6375e155b0b5352874
Jeff-Adler <Jeffm.adler@gmail.com>
2021-10-03 16:31:34 -0400

    removed db check from server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,5 @@
 import App from '@/src/app';
-import { PrismaClient } from '@prisma/client';
 
 const app = new App();
 
 app.listen();
-
-const prisma = new PrismaClient();
-
-(async function main() {
-  const result = await prisma.account.findMany();
-  console.log(result.length);
-  await prisma.$disconnect();
-})();
